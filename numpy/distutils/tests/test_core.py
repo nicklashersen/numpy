5838e6695ada29858c321b288e0f442dce8a5c72
Nicklas Hersén <nhersen@kth.se>
2019-02-18 19:52:04 +0100

    TST: Add configuration return branch test distutils.core.setup. Fix #46

--- numpy/distutils/tests/test_core.py
+++ numpy/distutils/tests/test_core.py
@@ -1,10 +1,16 @@
 from __future__ import division, print_function
 
 import pytest
+from unittest import mock
+
 from numpy.distutils.core import setup as core_setup
 
 class _lib_class:
     libraries = 1
+
+class _setup_return_class:
+    help = True
+    
 
 def test_setup_type_error():
     lib = _lib_class
@@ -12,3 +18,9 @@ def test_setup_type_error():
     attr = { 'ext_modules': [lib] }
     with pytest.raises(TypeError):
         core_setup(**attr)
+
+@mock.patch("numpy.distutils.core.old_setup", return_value=_setup_return_class, autospec=True)
+def test_setup_dist_help(mock_old_setup):
+    attr = { 'configuration': [] }
+
+    core_setup(**attr)
