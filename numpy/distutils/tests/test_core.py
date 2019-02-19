99e983c7aee43570eea50e67da61d6004bad3cff
Nicklas Hersén <nhersen@kth.se>
2019-02-19 12:21:55 +0100

    DOC: Add doc for setup tests.

--- numpy/distutils/tests/test_core.py
+++ numpy/distutils/tests/test_core.py
@@ -15,6 +15,10 @@ class _ext_string_lib:
     libraries = "somelib"
 
 def test_setup_type_error():
+    """
+    Test that setup ext_modules throws an Type error
+    if the ext module list isn't a sequence or string.
+    """
     lib = _lib_class
 
     attr = { 'ext_modules': [lib] }
@@ -23,12 +27,19 @@ def test_setup_type_error():
 
 @mock.patch("numpy.distutils.core.old_setup", return_value=_setup_return_class, autospec=True)
 def test_setup_dist_help(mock_old_setup):
+    """
+    Test the configuration help branch in setup. 
+    """
     attr = { 'configuration': [] }
 
     assert core_setup(**attr).help == True
 
 @mock.patch("numpy.distutils.core.old_setup", return_value=_setup_return_class, autospec=True)
 def test_setup_ext_library_string(mock_setup):
+    """
+    Test the ext_modules branch in setup for strings returns
+    the correct result.
+    """
     lib = _ext_string_lib
     attr = { 'ext_modules': [lib] }
     core_setup(**attr)
