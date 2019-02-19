2ed56515df89ca7bf27686f75a42bb2644e02ff0
Erik Johansson <eika19631@gmail.com>
2019-02-19 10:03:49 +0100

    Refactor to reduce CCN. Fix issue #66

--- numpy/f2py/crackfortran.py
+++ numpy/f2py/crackfortran.py
@@ -899,6 +899,244 @@ def _resolvenameargspattern(line):
         return m1.group('name'), m1.group('args'), None, None
     return None, [], None, None
 
+def _analyze_data_case(m, case, line):
+    """
+    Perform data line analysis.
+    """
+
+    global groupcounter, groupcache, previous_context
+
+    ll = []
+    dl = ''
+    il = ''
+    f = 0
+    fc = 1
+    inp = 0
+    for c in m.group('after'):
+        if not inp:
+            if c == "'":
+                fc = not fc
+            if c == '/' and fc:
+                f = f + 1
+                continue
+        if c == '(':
+            inp = inp + 1
+        elif c == ')':
+            inp = inp - 1
+        if f == 0:
+            dl = dl + c
+        elif f == 1:
+            il = il + c
+        elif f == 2:
+            dl = dl.strip()
+            if dl.startswith(','):
+                dl = dl[1:].strip()
+            ll.append([dl, il])
+            dl = c
+            il = ''
+            f = 0
+    if f == 2:
+        dl = dl.strip()
+        if dl.startswith(','):
+            dl = dl[1:].strip()
+        ll.append([dl, il])
+    vars = {}
+    if 'vars' in groupcache[groupcounter]:
+        vars = groupcache[groupcounter]['vars']
+    last_name = None
+    for l in ll:
+        l = [x.strip() for x in l]
+        if l[0][0] == ',':
+            l[0] = l[0][1:]
+        if l[0][0] == '(':
+            outmess(
+                'analyzeline: implied-DO list "%s" is not supported. Skipping.\n' % l[0])
+            continue
+        i = 0
+        j = 0
+        llen = len(l[1])
+        for v in rmbadname([x.strip() for x in markoutercomma(l[0]).split('@,@')]):
+            if v[0] == '(':
+                outmess(
+                    'analyzeline: implied-DO list "%s" is not supported. Skipping.\n' % v)
+                # XXX: subsequent init expressions may get wrong values.
+                # Ignoring since data statements are irrelevant for
+                # wrapping.
+                continue
+            fc = 0
+            while (i < llen) and (fc or not l[1][i] == ','):
+                if l[1][i] == "'":
+                    fc = not fc
+                i = i + 1
+            i = i + 1
+            if v not in vars:
+                vars[v] = {}
+            if '=' in vars[v] and not vars[v]['='] == l[1][j:i - 1]:
+                outmess('analyzeline: changing init expression of "%s" ("%s") to "%s"\n' % (
+                    v, vars[v]['='], l[1][j:i - 1]))
+            vars[v]['='] = l[1][j:i - 1]
+            j = i
+            last_name = v
+    groupcache[groupcounter]['vars'] = vars
+    if last_name is not None:
+        previous_context = ('variable', last_name, groupcounter)
+
+def _analyze_implicit_case(m, case, line):
+    """
+    Perform implicit line analysis.
+    """
+
+    global groupcache, groupcounter
+
+    if m.group('after').strip().lower() == 'none':
+        groupcache[groupcounter]['implicit'] = None
+    elif m.group('after'):
+        if 'implicit' in groupcache[groupcounter]:
+            impl = groupcache[groupcounter]['implicit']
+        else:
+            impl = {}
+        if impl is None:
+            outmess(
+                'analyzeline: Overwriting earlier "implicit none" statement.\n')
+            impl = {}
+        for e in markoutercomma(m.group('after')).split('@,@'):
+            decl = {}
+            m1 = re.match(
+                r'\s*(?P<this>.*?)\s*(\(\s*(?P<after>[a-z-, ]+)\s*\)\s*|)\Z', e, re.I)
+            if not m1:
+                outmess(
+                    'analyzeline: could not extract info of implicit statement part "%s"\n' % (e))
+                continue
+            m2 = typespattern4implicit.match(m1.group('this'))
+            if not m2:
+                outmess(
+                    'analyzeline: could not extract types pattern of implicit statement part "%s"\n' % (e))
+                continue
+            typespec, selector, attr, edecl = cracktypespec0(
+                m2.group('this'), m2.group('after'))
+            kindselect, charselect, typename = cracktypespec(
+                typespec, selector)
+            decl['typespec'] = typespec
+            decl['kindselector'] = kindselect
+            decl['charselector'] = charselect
+            decl['typename'] = typename
+            for k in list(decl.keys()):
+                if not decl[k]:
+                    del decl[k]
+            for r in markoutercomma(m1.group('after')).split('@,@'):
+                if '-' in r:
+                    try:
+                        begc, endc = [x.strip() for x in r.split('-')]
+                    except Exception:
+                        outmess(
+                            'analyzeline: expected "<char>-<char>" instead of "%s" in range list of implicit statement\n' % r)
+                        continue
+                else:
+                    begc = endc = r.strip()
+                if not len(begc) == len(endc) == 1:
+                    outmess(
+                        'analyzeline: expected "<char>-<char>" instead of "%s" in range list of implicit statement (2)\n' % r)
+                    continue
+                for o in range(ord(begc), ord(endc) + 1):
+                    impl[chr(o)] = decl
+        groupcache[groupcounter]['implicit'] = impl
+
+def _analyze_common_case(m, case, line):
+    """
+    Perform implicit line analysis.
+    """
+
+    global groupcounter, groupcache, previous_context
+
+    line = m.group('after').strip()
+    if not line[0] == '/':
+        line = '//' + line
+    cl = []
+    f = 0
+    bn = ''
+    ol = ''
+    for c in line:
+        if c == '/':
+            f = f + 1
+            continue
+        if f >= 3:
+            bn = bn.strip()
+            if not bn:
+                bn = '_BLNK_'
+            cl.append([bn, ol])
+            f = f - 2
+            bn = ''
+            ol = ''
+        if f % 2:
+            bn = bn + c
+        else:
+            ol = ol + c
+    bn = bn.strip()
+    if not bn:
+        bn = '_BLNK_'
+    cl.append([bn, ol])
+    commonkey = {}
+    if 'common' in groupcache[groupcounter]:
+        commonkey = groupcache[groupcounter]['common']
+    for c in cl:
+        if c[0] not in commonkey:
+            commonkey[c[0]] = []
+        for i in [x.strip() for x in markoutercomma(c[1]).split('@,@')]:
+            if i:
+                commonkey[c[0]].append(i)
+    groupcache[groupcounter]['common'] = commonkey
+    previous_context = ('common', bn, groupcounter)   
+
+def _analyze_parameter_case(m, case, line):
+    """
+    Perform parameter line analysis.
+    """
+
+    global groupcache, groupcounter, previous_context
+
+    edecl = groupcache[groupcounter]['vars']
+    ll = m.group('after').strip()[1:-1]
+    last_name = None
+    for e in markoutercomma(ll).split('@,@'):
+        try:
+            k, initexpr = [x.strip() for x in e.split('=')]
+        except Exception:
+            outmess(
+                'analyzeline: could not extract name,expr in parameter statement "%s" of "%s"\n' % (e, ll))
+            continue
+        params = get_parameters(edecl)
+        k = rmbadname1(k)
+        if k not in edecl:
+            edecl[k] = {}
+        if '=' in edecl[k] and (not edecl[k]['='] == initexpr):
+            outmess('analyzeline: Overwriting the value of parameter "%s" ("%s") with "%s".\n' % (
+                k, edecl[k]['='], initexpr))
+        t = determineexprtype(initexpr, params)
+        if t:
+            if t.get('typespec') == 'real':
+                tt = list(initexpr)
+                for m in real16pattern.finditer(initexpr):
+                    tt[m.start():m.end()] = list(
+                        initexpr[m.start():m.end()].lower().replace('d', 'e'))
+                initexpr = ''.join(tt)
+            elif t.get('typespec') == 'complex':
+                initexpr = initexpr[1:].lower().replace('d', 'e').\
+                    replace(',', '+1j*(')
+        try:
+            v = eval(initexpr, {}, params)
+        except (SyntaxError, NameError, TypeError) as msg:
+            errmess('analyzeline: Failed to evaluate %r. Ignoring: %s\n'
+                    % (initexpr, msg))
+            continue
+        edecl[k]['='] = repr(v)
+        if 'attrspec' in edecl[k]:
+            edecl[k]['attrspec'].append('parameter')
+        else:
+            edecl[k]['attrspec'] = ['parameter']
+        last_name = k
+    groupcache[groupcounter]['vars'] = edecl
+    if last_name is not None:
+        previous_context = ('variable', last_name, groupcounter)
 
 def analyzeline(m, case, line):
     global groupcounter, groupname, groupcache, grouplist, filepositiontext
@@ -1178,216 +1416,13 @@ def analyzeline(m, case, line):
         if last_name is not None:
             previous_context = ('variable', last_name, groupcounter)
     elif case == 'parameter':
-        edecl = groupcache[groupcounter]['vars']
-        ll = m.group('after').strip()[1:-1]
-        last_name = None
-        for e in markoutercomma(ll).split('@,@'):
-            try:
-                k, initexpr = [x.strip() for x in e.split('=')]
-            except Exception:
-                outmess(
-                    'analyzeline: could not extract name,expr in parameter statement "%s" of "%s"\n' % (e, ll))
-                continue
-            params = get_parameters(edecl)
-            k = rmbadname1(k)
-            if k not in edecl:
-                edecl[k] = {}
-            if '=' in edecl[k] and (not edecl[k]['='] == initexpr):
-                outmess('analyzeline: Overwriting the value of parameter "%s" ("%s") with "%s".\n' % (
-                    k, edecl[k]['='], initexpr))
-            t = determineexprtype(initexpr, params)
-            if t:
-                if t.get('typespec') == 'real':
-                    tt = list(initexpr)
-                    for m in real16pattern.finditer(initexpr):
-                        tt[m.start():m.end()] = list(
-                            initexpr[m.start():m.end()].lower().replace('d', 'e'))
-                    initexpr = ''.join(tt)
-                elif t.get('typespec') == 'complex':
-                    initexpr = initexpr[1:].lower().replace('d', 'e').\
-                        replace(',', '+1j*(')
-            try:
-                v = eval(initexpr, {}, params)
-            except (SyntaxError, NameError, TypeError) as msg:
-                errmess('analyzeline: Failed to evaluate %r. Ignoring: %s\n'
-                        % (initexpr, msg))
-                continue
-            edecl[k]['='] = repr(v)
-            if 'attrspec' in edecl[k]:
-                edecl[k]['attrspec'].append('parameter')
-            else:
-                edecl[k]['attrspec'] = ['parameter']
-            last_name = k
-        groupcache[groupcounter]['vars'] = edecl
-        if last_name is not None:
-            previous_context = ('variable', last_name, groupcounter)
+        _analyze_parameter_case(m, case, line)
     elif case == 'implicit':
-        if m.group('after').strip().lower() == 'none':
-            groupcache[groupcounter]['implicit'] = None
-        elif m.group('after'):
-            if 'implicit' in groupcache[groupcounter]:
-                impl = groupcache[groupcounter]['implicit']
-            else:
-                impl = {}
-            if impl is None:
-                outmess(
-                    'analyzeline: Overwriting earlier "implicit none" statement.\n')
-                impl = {}
-            for e in markoutercomma(m.group('after')).split('@,@'):
-                decl = {}
-                m1 = re.match(
-                    r'\s*(?P<this>.*?)\s*(\(\s*(?P<after>[a-z-, ]+)\s*\)\s*|)\Z', e, re.I)
-                if not m1:
-                    outmess(
-                        'analyzeline: could not extract info of implicit statement part "%s"\n' % (e))
-                    continue
-                m2 = typespattern4implicit.match(m1.group('this'))
-                if not m2:
-                    outmess(
-                        'analyzeline: could not extract types pattern of implicit statement part "%s"\n' % (e))
-                    continue
-                typespec, selector, attr, edecl = cracktypespec0(
-                    m2.group('this'), m2.group('after'))
-                kindselect, charselect, typename = cracktypespec(
-                    typespec, selector)
-                decl['typespec'] = typespec
-                decl['kindselector'] = kindselect
-                decl['charselector'] = charselect
-                decl['typename'] = typename
-                for k in list(decl.keys()):
-                    if not decl[k]:
-                        del decl[k]
-                for r in markoutercomma(m1.group('after')).split('@,@'):
-                    if '-' in r:
-                        try:
-                            begc, endc = [x.strip() for x in r.split('-')]
-                        except Exception:
-                            outmess(
-                                'analyzeline: expected "<char>-<char>" instead of "%s" in range list of implicit statement\n' % r)
-                            continue
-                    else:
-                        begc = endc = r.strip()
-                    if not len(begc) == len(endc) == 1:
-                        outmess(
-                            'analyzeline: expected "<char>-<char>" instead of "%s" in range list of implicit statement (2)\n' % r)
-                        continue
-                    for o in range(ord(begc), ord(endc) + 1):
-                        impl[chr(o)] = decl
-            groupcache[groupcounter]['implicit'] = impl
+        _analyze_implicit_case(m, case, line)
     elif case == 'data':
-        ll = []
-        dl = ''
-        il = ''
-        f = 0
-        fc = 1
-        inp = 0
-        for c in m.group('after'):
-            if not inp:
-                if c == "'":
-                    fc = not fc
-                if c == '/' and fc:
-                    f = f + 1
-                    continue
-            if c == '(':
-                inp = inp + 1
-            elif c == ')':
-                inp = inp - 1
-            if f == 0:
-                dl = dl + c
-            elif f == 1:
-                il = il + c
-            elif f == 2:
-                dl = dl.strip()
-                if dl.startswith(','):
-                    dl = dl[1:].strip()
-                ll.append([dl, il])
-                dl = c
-                il = ''
-                f = 0
-        if f == 2:
-            dl = dl.strip()
-            if dl.startswith(','):
-                dl = dl[1:].strip()
-            ll.append([dl, il])
-        vars = {}
-        if 'vars' in groupcache[groupcounter]:
-            vars = groupcache[groupcounter]['vars']
-        last_name = None
-        for l in ll:
-            l = [x.strip() for x in l]
-            if l[0][0] == ',':
-                l[0] = l[0][1:]
-            if l[0][0] == '(':
-                outmess(
-                    'analyzeline: implied-DO list "%s" is not supported. Skipping.\n' % l[0])
-                continue
-            i = 0
-            j = 0
-            llen = len(l[1])
-            for v in rmbadname([x.strip() for x in markoutercomma(l[0]).split('@,@')]):
-                if v[0] == '(':
-                    outmess(
-                        'analyzeline: implied-DO list "%s" is not supported. Skipping.\n' % v)
-                    # XXX: subsequent init expressions may get wrong values.
-                    # Ignoring since data statements are irrelevant for
-                    # wrapping.
-                    continue
-                fc = 0
-                while (i < llen) and (fc or not l[1][i] == ','):
-                    if l[1][i] == "'":
-                        fc = not fc
-                    i = i + 1
-                i = i + 1
-                if v not in vars:
-                    vars[v] = {}
-                if '=' in vars[v] and not vars[v]['='] == l[1][j:i - 1]:
-                    outmess('analyzeline: changing init expression of "%s" ("%s") to "%s"\n' % (
-                        v, vars[v]['='], l[1][j:i - 1]))
-                vars[v]['='] = l[1][j:i - 1]
-                j = i
-                last_name = v
-        groupcache[groupcounter]['vars'] = vars
-        if last_name is not None:
-            previous_context = ('variable', last_name, groupcounter)
+        _analyze_data_case(m, case, line)
     elif case == 'common':
-        line = m.group('after').strip()
-        if not line[0] == '/':
-            line = '//' + line
-        cl = []
-        f = 0
-        bn = ''
-        ol = ''
-        for c in line:
-            if c == '/':
-                f = f + 1
-                continue
-            if f >= 3:
-                bn = bn.strip()
-                if not bn:
-                    bn = '_BLNK_'
-                cl.append([bn, ol])
-                f = f - 2
-                bn = ''
-                ol = ''
-            if f % 2:
-                bn = bn + c
-            else:
-                ol = ol + c
-        bn = bn.strip()
-        if not bn:
-            bn = '_BLNK_'
-        cl.append([bn, ol])
-        commonkey = {}
-        if 'common' in groupcache[groupcounter]:
-            commonkey = groupcache[groupcounter]['common']
-        for c in cl:
-            if c[0] not in commonkey:
-                commonkey[c[0]] = []
-            for i in [x.strip() for x in markoutercomma(c[1]).split('@,@')]:
-                if i:
-                    commonkey[c[0]].append(i)
-        groupcache[groupcounter]['common'] = commonkey
-        previous_context = ('common', bn, groupcounter)
+        _analyze_common_case(m, case, line)
     elif case == 'use':
         m1 = re.match(
             r'\A\s*(?P<name>\b[\w]+\b)\s*((,(\s*\bonly\b\s*:|(?P<notonly>))\s*(?P<list>.*))|)\s*\Z', m.group('after'), re.I)
